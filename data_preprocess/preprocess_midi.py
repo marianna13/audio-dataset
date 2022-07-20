a2adf44309e6bb5c9691f3899524edf43804d2c9
Ubuntu <ubuntu@ip-172-31-86-225.ec2.internal>
2022-07-20 14:57:03 +0000

    updated scripts

--- data_preprocess/preprocess_midi.py
+++ data_preprocess/preprocess_midi.py
@@ -20,37 +20,51 @@ def process_data(filenames, rng, output_dir, audio_dir, dataset_name):
     filenames = filenames[start:end]
     file_id = 0
     for filename in tqdm(filenames, total=len(filenames)):
+        
         midi_path = f'{audio_dir}/{filename}'
         audio_path = midi_path.replace('.mid', 'flac')
-        text = audio_path.split('/')[-1].replace('.mid', '')
-        text = text.replace('_', ' ')
-        text = text.replace('.flac', '')
-        text = f'{text}, MIDI'
-        audio_json = {
-            'text': [text],
-            'tag': [filename],
-            'original_data': {
-                'title': dataset_name
-            },
+        subfolders = filename.replace('.mid','').split('___')[:-1]
+        
+        tag = filename.replace('.mid','').split('___')[-1].replace('.', ' ').replace('_',' ')
+        
+        text = ' '.join(subfolders).replace('_', ' ')
+        text = f'{text} MIDI version'
+        if not text.isdigit():
+            audio_json = {
+                'text': [text],
+                'tag': [tag],
+                'original_data': {
+                    'title': dataset_name,
+                    'Description': '130k Rock Songs MIDI files',
+                    'subfolders': subfolders,
+                    'filename':filename
+                },
 
-        }
-        audio_json_save_path = f'{output_dir}/{file_id}.json'
-        audio_save_path = f'{output_dir}/{file_id}.flac'
-        midi_to_audio(midi_file=midi_path,
-                      audio_file=audio_path.replace('.mid', 'flac'))
+            }
+            audio_json_save_path = f'{output_dir}/{file_id}.json'
+            audio_save_path = f'{output_dir}/{file_id}.flac'
+            midi_to_audio(midi_file=midi_path,
+                        audio_file=audio_path.replace('.mid', 'flac'))
 
-        result = subprocess.run(['ffprobe', '-v', 'error', '-show_entries', 'format=duration', '-of',
-                                 'default=noprint_wrappers=1:nokey=1', audio_path], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-        duration = str(result.stdout)[2:-3]
-        if duration != 'N/A':
-            duration = int(float(duration))
-            for j in range(0, duration-10, 10):
-                audio_json_save_path = f'{output_dir}/{file_id}.json'
-                audio_save_path = f'{output_dir}/{file_id}.flac'
-                json_dump(audio_json, audio_json_save_path)
-                audio_to_flac(audio_path, audio_save_path,
-                              AUDIO_SAVE_SAMPLE_RATE, segment_start=j,  segment_end=j+10)
-                file_id += 1
+            result = subprocess.run(['ffprobe', '-v', 'error', '-show_entries', 'format=duration', '-of',
+                                    'default=noprint_wrappers=1:nokey=1', audio_path], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+            
+            duration = str(result.stdout)[2:-3]
+            if duration != 'N/A':
+                try:
+                    duration = int(float(duration))
+                    for j in range(0, duration-10, 10):
+                        audio_json_save_path = f'{output_dir}/{file_id}.json'
+                        audio_save_path = f'{output_dir}/{file_id}.flac'
+                        json_dump(audio_json, audio_json_save_path)
+                        audio_to_flac(audio_path, audio_save_path,
+                                    AUDIO_SAVE_SAMPLE_RATE, segment_start=j,  segment_end=j+10)
+                        file_id += 1
+                except ValueError:
+                    continue
+
+            else:
+                continue
 
         else:
             continue
@@ -59,6 +73,9 @@ def process_data(filenames, rng, output_dir, audio_dir, dataset_name):
 def preprocess(audio_dir, output_dir, dataset_name, num_process):
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
+    
+    if not os.path.exists(audio_dir):
+        os.makedirs(audio_dir)
 
     filenames = os.listdir(audio_dir)
     N = len(filenames)
@@ -84,7 +101,7 @@ def preprocess(audio_dir, output_dir, dataset_name, num_process):
 
 
 if __name__ == '__main__':
-    dataset_name = 'test_midi'
-    audio_dir = f'{dataset_name}/AUDIO'
+    dataset_name = '130000_Pop_Rock_Classical_Videogame_EDM_MIDI_Archive[6_19_15]'
+    audio_dir = f'raw_datasets/{dataset_name}'
     output_dir = f'processed/{dataset_name}'
-    preprocess(audio_dir, output_dir, dataset_name, num_process=4)
+    preprocess(audio_dir, output_dir, dataset_name, num_process=70)
