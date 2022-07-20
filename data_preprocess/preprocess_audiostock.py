3206615a5cf33ef107a6013496b3ebd64cd02441
Ubuntu <ubuntu@ip-172-31-86-225.ec2.internal>
2022-07-20 14:56:17 +0000

    updated scripts

--- data_preprocess/preprocess_audiostock.py
+++ data_preprocess/preprocess_audiostock.py
@@ -1,32 +1,72 @@
-
 from tqdm import tqdm
 import os
 import pandas as pd
 import sys
+import requests
 sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
 
-if __name__ == '__main__':
-    from utils.file_utils import json_load, json_dump
+def download_and_save_file(URL, audio_dir):
+    headers = {
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.36',
+        'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+        'referer': 'https://dictionary.cambridge.org/dictionary/english',
+        'accept-encoding': 'gzip, deflate, br',
+        'accept-language': 'en-US,en;q=0.9,',
+        'cookie': 'prov=6bb44cc9-dfe4-1b95-a65d-5250b3b4c9fb; _ga=GA1.2.1363624981.1550767314; __qca=P0-1074700243-1550767314392; notice-ctt=4%3B1550784035760; _gid=GA1.2.1415061800.1552935051; acct=t=4CnQ70qSwPMzOe6jigQlAR28TSW%2fMxzx&s=32zlYt1%2b3TBwWVaCHxH%2bl5aDhLjmq4Xr',
+    }
+    doc = requests.get(URL, headers=headers)
+    file_name = URL.split('/')[-1]
+    audio_path = f'{audio_dir}/{file_name}'
+    with open(audio_path, 'wb') as f:
+        f.write(doc.content)
+    return audio_path
+
+
+def preprocess(meta_dir, output_dir, audio_dir):
+    from utils.file_utils import json_dump
     from utils.audio_utils import audio_to_flac
     from utils.dataset_parameters import AUDIO_SAVE_SAMPLE_RATE
-    dataset_name = 'audiostock'
-    data_dir = f'/mnt/marianna/clap/raw_datasets/{dataset_name}'
-    output_dir = f'/mnt/marianna/clap/processed_datasets/{dataset_name}'
+
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
 
-    audio_dir = f'{data_dir}/audio'
-    meta_dir = f'/mnt/marianna/clap/raw_datasets/audiostock/meta/meta.csv'
+    if not os.path.exists(audio_dir):
+        os.makedirs(audio_dir)
 
     meta = pd.read_csv(meta_dir)
-    filenames, texts = meta['fname'].values, meta['text'].values
+
     file_id = 0
-    for filename, text in tqdm(zip(filenames, texts), total=len(filenames)):
-        audio_path = f'{audio_dir}/{filename}'
-        audio_json = {'text': text, 'tag': 'audiostock'}
+    for row in tqdm(meta.iterrows(), total=len(meta)):
+        index, title,impression, scene,purpose,tags,audio_url,audio_size  = row[1].values
+        text = title
+
+        audio_path = download_and_save_file(audio_url, audio_dir)
+        audio_json = {
+            'text': [text],
+            'tag': tags.split(','),
+            'original_data': {
+                'title': 'Audiostock dataset',
+                'Description': 'Sound effects scraped from the audiostock.net website',
+                'URL': audio_url,
+                'scene': scene,
+                'purpose':purpose,
+                'impression':impression,
+                'audio_size': audio_size
+            }
+        }
         audio_json_save_path = f'{output_dir}/{file_id}.json'
         audio_save_path = f'{output_dir}/{file_id}.flac'
-        print(audio_save_path, audio_path)
+
         json_dump(audio_json, audio_json_save_path)
-        audio_to_flac(audio_path, audio_save_path, AUDIO_SAVE_SAMPLE_RATE)
+        audio_to_flac(audio_path, audio_save_path,
+                      AUDIO_SAVE_SAMPLE_RATE, no_log=True)
         file_id += 1
+
+
+if __name__ == '__main__':
+    dataset_name = 'audio_stock'
+    output_dir = f'processed/{dataset_name}'
+    meta_dir = 'audiostock_meta.csv'
+    audio_dir = f'processed/{dataset_name}/AUDIO'
+    preprocess(meta_dir=meta_dir, output_dir=output_dir,
+               audio_dir=audio_dir)
