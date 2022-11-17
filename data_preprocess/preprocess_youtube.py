d6ce134c927a4a3b0df8083c69630a9e0b47a485
marianna <mariana13019940@gmail.com>
2022-11-17 14:02:55 +0000

    add audiostock music script & data card

--- data_preprocess/preprocess_youtube.py
+++ data_preprocess/preprocess_youtube.py
@@ -40,10 +40,7 @@ def process_channel(video_urls, output_dir, audio_dir, channel_name=None):
     
     for video_url in tqdm(video_urls, total=len(video_urls)):
 
-        if file_id == 2:
-            break
         yt = YouTube(video_url)
-        print(video_url)
 
         try:
             caption = yt.captions['en-US']
@@ -52,60 +49,64 @@ def process_channel(video_urls, output_dir, audio_dir, channel_name=None):
                 caption = yt.captions['en']
             except:
                 continue
+        yt = video_url.split('/')[-1].split('=')[-1]
+        with open(f'{output_dir}/{yt}.txt', 'w') as f:
+            f.write('0')
+
                 # try:
                 #     caption = yt.captions['a.en']
                 # except kedyError:
                 #     continue
 
-        title, wav_path = download_from_URL(video_url,audio_dir)
+        # title, wav_path = download_from_URL(video_url,audio_dir)
 
-        js = xmltodict.parse(caption.xml_captions)
-        timedtext = js['timedtext']['body']['p']
-        filename = wav_path
-        for dic in timedtext:
-            audio_path = audio_dir+filename
-            try: 
-                sf.read(audio_path)
-            except:
-                continue
-            audio_json_save_path = f'{output_dir}/{file_id}.json'
-            audio_save_path = f'{output_dir}/{file_id}.flac'
-            try:
-                t = int(dic['@t'])/1000
-                d = int(dic['@d'])/1000
-                end = round(t+d, 3)
-                text = dic['#text']
-                if '[Music]' in text or 'Transcriber' in text or 'Translator' in text:
-                    continue
-                text = text.replace('\n', '').replace(u'\u2019', "'").replace('\ufeff', '')
-                caption = f'a person saying "{text}"'
-                if '[' in text and ']' in text:
-                    caption = text.replace('[', '').replece(']', '')
+        # js = xmltodict.parse(caption.xml_captions)
+        # timedtext = js['timedtext']['body']['p'][:3]
+        # filename = wav_path
+        # for dic in timedtext:
+        #     audio_path = audio_dir+filename
+        #     try: 
+        #         sf.read(audio_path)
+        #     except:
+        #         continue
+        #     audio_json_save_path = f'{output_dir}/{file_id}.json'
+        #     audio_save_path = f'{output_dir}/{file_id}.flac'
+        #     try:
+        #         t = int(dic['@t'])/1000
+        #         d = int(dic['@d'])/1000
+        #         end = round(t+d, 3)
+        #         text = dic['#text']
+        #         if '[Music]' in text or 'Transcriber' in text or 'Translator' in text:
+        #             continue
+        #         text = text.replace('\n', '').replace(u'\u2019', "'").replace('\ufeff', '')
+        #         caption = f'a person saying "{text}"'
+        #         if '[' in text and ']' in text:
+        #             caption = text.replace('[', '').replece(']', '')
 
-                tags = [title, f'{channel_name} Youtube Video'] if channel_name else [title]
-                audio_json = {
-                    'text': [caption],
-                    'start':t,
-                    'end':end,
-                    'duration':d,
-                    'url':f'{video_url}&t={t}s'
-                    # 'tag': [title, f'{channel_name} Youtube Video'],
-                    # 'original_data': {
-                    #     'title': 'YT dataset',
-                    #     'filename':filename,
-                    #     'url': video_url,
-                    #     'duration':d,
-                    #     'start':t,
-                    #     'end':end
-                    #     },
-                        }
-                json_dump(audio_json, audio_json_save_path)
-                audio_to_flac(audio_path, audio_save_path,
-                            AUDIO_SAVE_SAMPLE_RATE, segment_start=t,  segment_end=end)
+        #         tags = [title, f'{channel_name} Youtube Video'] if channel_name else [title]
+        #         audio_json = {
+        #             'text': [caption],
+        #             'start':t,
+        #             'end':end,
+        #             'duration':d,
+        #             'url':f'{video_url}&t={t}s'
+        #             # 'tag': [title, f'{channel_name} Youtube Video'],
+        #             # 'original_data': {
+        #             #     'title': 'YT dataset',
+        #             #     'filename':filename,
+        #             #     'url': video_url,
+        #             #     'duration':d,
+        #             #     'start':t,
+        #             #     'end':end
+        #             #     },
+        #                 }
+        #         json_dump(audio_json, audio_json_save_path)
+        #         audio_to_flac(audio_path, audio_save_path,
+        #                     AUDIO_SAVE_SAMPLE_RATE, segment_start=t,  segment_end=end)
 
-                file_id +=1
-            except KeyError:
-                continue
+        #         file_id +=1
+            # except KeyError:
+            #     continue
 
 def preprocess(channel_name, num_process):
 
@@ -117,7 +118,7 @@ def preprocess(channel_name, num_process):
     if not os.path.exists(audio_dir):
             os.makedirs(audio_dir)
     # c = Channel(channel_url)
-    df = pd.read_parquet('sample1k.parquet').sample(100)
+    df = pd.read_parquet('acav0_10000_acav1.parquet').sample(1000)
     df['URL'] = df.ID.apply(lambda x: 'https://www.youtube.com/watch?v='+x)
     video_urls = df.URL.values
     # video_urls = c.video_urls
@@ -144,4 +145,4 @@ def preprocess(channel_name, num_process):
     print(f'Processed in {round(e-s, 2)} seconds')
 
 if __name__=='__main__':
-    preprocess(channel_name='test', num_process=10)
+    preprocess(channel_name='test2', num_process=10)
