e648d3ff054f377a6b792aef055d0d84e8987693
marianna <mariana13019940@gmail.com>
2023-02-16 08:32:00 +0000

    upadte fma script

--- data_preprocess/preprocess_fma.py
+++ data_preprocess/preprocess_fma.py
@@ -4,10 +4,12 @@ import os
 import subprocess
 import pandas as pd
 import multiprocessing as mp
+import threading as td
 import time
 import sys
 import re
 import shutil
+import py7zr
 import zipfile
 import random
 from split_and_rename import split_dataset
@@ -18,7 +20,7 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
 def get_audio_path(audio_dir, track_id):
     """
     """
-    tid_str = audio_dir.split('/')[-1]+str(track_id)
+    tid_str = f'{track_id:06}'
     return os.path.join(audio_dir, tid_str + '.mp3')
 
 
@@ -94,60 +96,62 @@ def process_data(track_ids, texts, artists, albums, output_dir, audio_dir):
         song_info = ', '.join(song_info)
         extra_info = ', '.join(extra_info)
         caption = f'playing song {song_info}, of which {extra_info}'
-        tag = [title]
         duration = int(duration)
-        for j in range(0, duration-10, 10):
-            original_data = {
-                'title': 'FMA: A Dataset For Music Analysis',
-                'description':"Free Music Archive (FMA), an open and easily accessible dataset suitable for evaluating several tasks in MIR, a field concerned with browsing, searching, and organizing large music collections.",
-                'license':'MIT License',
-                'filename':filename.split('/')[-1],
-                'genre': str(genre),
-                'album': str(album),
-                'duration':duration,
-                'composer': str(composer),
-                'date_recorded': str(date_recorded),
-                'language_code': language_code,
-                'split':[j,j+10]
-                }
-            audio_json = {
-                'text': caption, 
-                'tag': tag, 
-                'original_data':original_data,
-                }
+        # for j in range(0, duration-10, 10):
+        original_data = {
+            'title': 'FMA: A Dataset For Music Analysis',
+            'description':"Free Music Archive (FMA), an open and easily accessible dataset suitable for evaluating several tasks in MIR, a field concerned with browsing, searching, and organizing large music collections.",
+            'license':'MIT License',
+            'filename':filename.split('/')[-1],
+            'genre': str(genre),
+            'album': str(album),
+            'song_title': str(title),
+            'artist':str(artist),
+            'duration':duration,
+            'composer': str(composer),
+            'date_recorded': str(date_recorded),
+            'language_code': language_code,
+            }
+        audio_json = {
+            'text': caption, 
+            'original_data':original_data,
+            }
 
-            audio_json_save_path = f'{output_dir}/{file_id}.json'
-            audio_save_path = f'{output_dir}/{file_id}.flac'
-            json_dump(audio_json, audio_json_save_path)
-            audio_to_flac(audio_path, audio_save_path,
-                          AUDIO_SAVE_SAMPLE_RATE, segment_start=j,  segment_end=j+10)
-            file_id += 1
+        audio_json_save_path = f'{output_dir}/{file_id}.json'
+        audio_save_path = f'{output_dir}/{file_id}.flac'
+        json_dump(audio_json, audio_json_save_path)
+        audio_to_flac(audio_path, audio_save_path,
+                        AUDIO_SAVE_SAMPLE_RATE)
+        file_id += 1
 
 def process_subfolder(subfolder, dataset_name):
-    output_dir = f'/home/ubuntu/marianna/clap/processed_datasets/{dataset_name}/{subfolder}'
+    output_dir = f'{dataset_name}_full/'
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
 
-    audio_dir = f'/home/ubuntu/marianna/clap/raw_datasets/fma_small/fma_small/{subfolder}'
-    meta_dir = f'/home/ubuntu/marianna/clap/raw_datasets/fma_metadata/tracks.csv'
+    audio_dir = f'FMA'
+    meta_dir = f'FMA_metadata/fma_metadata/fma_metadata/tracks.csv'
+    audios = os.listdir(audio_dir)
 
-    track_ids = [int(audio_name.replace('.mp3','')[3:]) for audio_name in os.listdir(audio_dir)]
+    track_ids = [int(audio_name.replace('.mp3','')) for audio_name in audios]
+    track_ids.sort()
     meta = load(meta_dir)
-    tracks = meta['track'].iloc[track_ids]
-
-    artists = meta['artist']['name'].iloc[track_ids]
-    albums = meta['album']['title'].iloc[track_ids].values
+    tracks = meta['track']
+    tracks = tracks[tracks.index.isin(track_ids)]
+    artists = meta['artist']['name']
+    artists = artists[artists.index.isin(track_ids)]
+    albums = meta['album']['title']
+    albums = albums[albums.index.isin(track_ids)].values
     artists = artists.values
+
     texts = tracks[['title', 'genre_top', 'duration', 'composer', 'language_code', 'date_created']].values
-    N = len(os.listdir(audio_dir))
+    N = len(audios)
     
-    texts = texts
-    artists = artists
 
     file_id = 0
-    num_process = 20
+    num_process = 50
     processes = []
-    out_dirs = [f'{output_dir}/{i} 'for i in range(num_process)]
+    out_dirs = [f'{output_dir}/{i}' for i in range(num_process)]
     for out_dir in out_dirs:
         if not os.path.exists(out_dir):
             os.makedirs(out_dir)
@@ -157,7 +161,7 @@ def process_subfolder(subfolder, dataset_name):
     s = time.time()
     for rng, out_dir in zip(rngs, out_dirs):
         start, end = rng
-        p = mp.Process(target=process_data, args=[
+        p = td.Thread(target=process_data, args=[
                        track_ids[start:end], texts[start:end],  artists[start:end], albums[start:end], out_dir, audio_dir])
         p.start()
         processes.append(p)
@@ -169,34 +173,24 @@ def process_subfolder(subfolder, dataset_name):
 
 
 if __name__ == '__main__':
-    from utils.merge_dirs import merge_dirs
-    from utils.unzip import unzip_file
+    
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--start', type=int)
+    parser.add_argument('--end', type=int)
+    args = parser.parse_args()
+    start = args.start
+    end = args.end
     dataset_name = 'fma'
-    src_file = 'fma_full.zip'
-    archive = zipfile.ZipFile(src_file)
-    subfolders = sorted(set([os.path.dirname(x) for x in archive.namelist()]))
+    src_file = 'fma_full.zip'    # archive = py7zr.SevenZipFile(src_file)
+    subfolders = [f'/fma_full/{i:03d}' for i in range(start, end)]
 
-    out = subprocess.check_output(['aws', 's3', 'ls', 's3://s-laion-audio/webdataset_tar/FMA/'])
-    done = [d.replace(' ', '').replace('\n','').replace('PRE','') for d in out.decode("utf-8").split('\n')]
-    done = [s.replace('/', '') for s in done]
-    done = [d for d in done if d!='']
 
-    # subfolders = os.listdir('/home/ubuntu/marianna/clap/raw_dataset/fma_full/fma_full')
-    subfolders = [subfolder.replace('fma_small/', '') for subfolder in subfolders]
-    subfolders = [s for s in subfolders if s!='fma_small']
-    # done_subfolders = os.listdir('raw_dataset/fma_small')
-    # done_subfolders.extend(done)
-    # subfolders = [s for s in subfolders if s not in done_subfolders]
-
-    for subfolder in subfolders:
-        unzip_file(src_file, target_dir='raw_datasets', folder='fma_small'+subfolder)
-        output_dir = process_subfolder(subfolder, dataset_name)
-        merge_dirs(root_dir=output_dir, to_rename=True)
-        split_dataset(data_dir=output_dir)
-        tar_dir = 'processed/FMA'
-        subprocess.run(['python', '/home/ubuntu/marianna/clap/audio-dataset/utils/make_tar.py', '--input', output_dir, '--output', f'{tar_dir}/{subfolder}'])
-        subprocess.run(['aws', 's3', 'cp', 'processed', 's3://s-laion-audio/webdataset_tar/', '--recursive'])
-        shutil.rmtree(output_dir)
-        shutil.rmtree(f'{tar_dir}/{subfolder}')
-        shutil.rmtree(f'/home/ubuntu/marianna/clap/raw_datasets/fma_full/fma_full/{subfolder}')
-
+    # for subfolder in subfolders:
+    #     print(subfolder)
+    #     cmd = f'7z x fma_full.zip -oFMA {subfolder[1:]}/*'
+    #     os.system(cmd)
+    #     output_dir = process_subfolder(subfolder, dataset_name)
+    #     os.system(f'rm -rf FMA/{subfolder}')
+       
